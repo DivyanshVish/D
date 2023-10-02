f48c3a2122d36b0d14b60f32f4edbf05f97c0c37
Divyansh Vishwakarma <ansh1111smith@gmail.com>
2023-10-02 21:25:58 +0530

    Initial Commit

--- 1859. Sorting the Sentence.cpp
+++ 1859. Sorting the Sentence.cpp
@@ -1,21 +1,26 @@
-class Solution {
+
+class Solution
+{
 public:
-    string sortSentence(string s) {
-        
+    string sortSentence(string s)
+    {
+
         /* ---------- Create a pos vector to store the position of the word --------- */
-        vector<string> pos(10,"");  
-        for(int i=0; i<s.size(); i++){
+        vector<string> pos(10, "");
+        for (int i = 0; i < s.size(); i++)
+        {
             /* ---------------- Create a temp string to store the word --------------- */
-            string temp; 
+            string temp;
             /* -------------- If s[i] is not digit then add s[i] to word ------------- */
-            while(!isdigit(s[i])) 
-                temp += s[i++]; 
-                /* ----------------- When digit occurs, add its position ----------------- */
-            pos[s[i++]-'0'] = temp; 
+            while (!isdigit(s[i]))
+                temp += s[i++];
+            /* -----------------ve When digit occurs, add its position ----------------- */
+            pos[s[i++] - '0'] = temp;
         }
         string ans;
-        for(int i=1; i<10; i++){
-            if(pos[i].length()>0)
+        for (int i = 1; i < 10; i++)
+        {
+            if (pos[i].length() > 0)
                 ans += pos[i] + ' ';
         }
         ans.pop_back();
--- Pattern.cpp
+++ Pattern.cpp
@@ -7,16 +7,49 @@ using namespace std;
 /* ----------- 2) For the Inner Loop, Count the number of columns and somehow connect with rows ----------- */
 /* ----------------- 3) Print the pattern in the Inner Loop ----------------- */
 /* -------------------- 4) Observe the symmetric [Optional] ------------------- */
-
-int main()
+void printPattern1(int n)
 {
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < n; j++)
         {
             cout << "* ";
         }
         cout << endl;
+    }
+}
+void Pattern3(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void Pattern2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout <<"* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        printPattern1(n);
     }
 
     return 0;
